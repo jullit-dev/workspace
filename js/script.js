345c49cf0a37420be2563adf8eaa1956faa48742
Julia Kuznetsova <grace_foto@mail.ru>
2024-06-18 01:37:56 +0300

    api for cities

--- js/script.js
+++ js/script.js
@@ -1,7 +1,37 @@
-const citySelect = document.getElementById('city');
-const cityChoices = new Choices(citySelect, {
-  searchEnabled: false,
-  itemSelectText: '',
-});
+const API_URL = 'https://workspace-methed.vercel.app/';
+const LOCATION_URL = 'api/locations';
 
+const getData = async (url, cbSuccess, cbError) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    cbSuccess(data);
+  } catch (err) {
+    cbError
+  }
+}
 
+const init = () => {
+  // Select
+  const citySelect = document.getElementById('city');
+  const cityChoices = new Choices(citySelect, {
+    itemSelectText: '',
+  });
+
+  // Api for city
+  getData(
+    `${API_URL}${LOCATION_URL}`, 
+    (locationData) => {
+      console.log(locationData);
+      const locations = locationData.map((location) => ({
+        value:location,
+      }));
+      cityChoices.setChoices(locations, "value", true);
+    },
+    (err) => {
+      console.log(err);
+    },
+  );
+}
+
+init()
